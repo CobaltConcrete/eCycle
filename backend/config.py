409c3dfac1bdb55ff30d879b18a67a6cd514c79c
CobaltConcrete <ganqingrong55@gmail.com>
2025-08-21 15:55:58 +0800

    [TEST] gitignore 2

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,8 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:Duckie51020@localhost/ecycle"
+    SQLALCHEMY_DATABASE_URI = os.getenv('REACT_APP_DATABASE_URL', 'postgresql://postgres:password@localhost/ecycle')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "Duckie51020"
+    SECRET_KEY = os.getenv('REACT_APP_DATABASE_PASSWORD')  # Change this for production
+
+#TESTETS
